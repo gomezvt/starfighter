cf7151406b59b6b07ec1f5d6d9a7620b3764abb3
gomezvt <greg@gomez.ws>
2020-02-05 19:22:35 -0500

    Add store files

---
+++ starfighter/Store.swift
@@ -0,0 +1,17 @@
+//
+//  Store.swift
+//  starfighter
+//
+//  Created by Greg on 2/5/20.
+//  Copyright © 2020 Caveman Games. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+import GameplayKit
+import AVFoundation
+import StoreKit
+
+class Store: SKScene, SKPhysicsContactDelegate {
+
+}
